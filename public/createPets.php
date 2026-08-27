4301eb2ea1a079afea1114627be5fc3876b9d984
DokeshiLab1Pc1 <isack_santana@estudante.sesisenai.org.br>
2026-08-27 09:04:21 -0300

    add - fetch_assoc

--- public/createPets.php
+++ public/createPets.php
@@ -3,11 +3,12 @@
 
 include("../infra/connection.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $petNome = $_POST['petNome'];
     $petRaca = $_POST['petRaca'];
     $petIdade = $_POST['petIdade'];
+    $clienteId = $_POST['clinteId'];
 
     $stmt = $conexao->prepare("INSERT INTO Pet (nome, raca, idade) VALUES (?, ?, ?)");
     $stmt->bind_param("ssi", $petNome, $petRaca, $petIdade);
@@ -25,22 +26,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
     <form method="POST">
 
         <input type="text" name="petNome"><br>
         <input type="text" name="petRaca"><br>
         <input type="number" name="petIdade"><br>
+        <label for="clinteId">Cliente:</label>
+        <select name="clinteId" id="clinteId">
+            <option value="0">Selecione um clinte</option>
+            <?php
+            $clientes = $conn->query("SELECT * FROM Cliente");
+            while ($cliente = $clientes->fetch_assoc()) {
+                ?>
+                <option value="<?php echo $cliente['clienteId']; ?>"><?php echo $cliente['clienteNome']; ?></option>
+                <?php
+            } ?>
+        </select>
         <button type="submit">Create Pet</button>
         <button><a href="../index.php">Voltar</a></button>
 
     </form>
 
 </body>
+
 </html>
